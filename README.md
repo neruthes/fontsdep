a5e3e6cbc9db16b3434dcb836319c59c6d0df782
Neruthes <i@neruthes.xyz>
2026-01-27 05:09:20 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fontsdep
+
 Per-repository on-demand font dependency installer
+
+
+# Copyright
+
+Copyright (c) 2026 Neruthes. Released with the GNU GPL 2.0 license.
+
---
+++ src/main/fontsdep.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+
+cat >/dev/null << EOFEOFEOF
+[MICRO DOCUMENTATION]
+
+Workflow when running this script:
+
+- Get config from package.json or somewhere else.
+- Create directories.
+- Resolve list of font files.
+- Skip some existing fonts files or download new ones.
+EOFEOFEOF
+
+
+
+
+
+# =====================================================================
+# Stage: Startup initialization
+# =====================================================================
+function _die() {
+    echo "[FATAL] $2"
+    exit "$1"
+}
+function check_bin () {
+    if ! command -v "$1" >/dev/null; then
+        die 1 "CANNOT FIND $1"
+    fi
+}
+check_bin jq
+check_bin wget
+
+
+
+
+# =====================================================================
+# Stage: Get config
+# =====================================================================
+
+### Currently only supports package.json
+[[ ! -e package.json ]] && _die 2 "Cannot find 'package.json' !"
+
+config_fontsdir="$(jq -r    .fontsdep.dest      package.json)"
+[[ "$config_fontsdir" == null ]] && config_fontsdir=_fontsdir
+echo "config_fontsdir=$config_fontsdir"
+
+config_fontslist="$(jq -r   .fontsdep.list[]    package.json)"
+[[ "$config_fontslist" == null ]] || [[ -z "$config_fontslist" ]] && _die 3 "Fonts list not configured!"
+
+config_cachetime="$(jq -r   .fontsdep.cacheLength    package.json)"
+[[ "$config_cachetime" == null ]] && config_cachetime=7
+echo "config_cachetime=$config_cachetime"
+
+
+function _reolve_font_url () {
+    input_url="$1"
+    item_ns="$(cut -d: -f1 <<< "$input_url")"
+    item_path="$(cut -d: -f2- <<< "$input_url")"
+    calculated_prefix=NULL
+    case $item_ns in
+        CTAN )
+            # calculated_prefix=https://mirrors.tuna.tsinghua.edu.cn/CTAN/fonts/
+            calculated_prefix=https://mirrors.ctan.org/fonts/
+            ;;
+    esac
+    case $calculated_prefix in
+        NULL )
+            return 1
+            ;;
+        * )
+            echo "$calculated_prefix$item_path"
+            return 0
+            ;;
+    esac
+}
+
+
+
+
+# =====================================================================
+# Stage: Download fonts
+# =====================================================================
+function _decider_skip_download_task() {
+    ### If file is new enough, return 0, else return 1
+    local local_path="$1"
+    if [[ ! -f "$local_path" ]]; then
+        return 1
+    fi
+    if find "$local_path" -mtime -7 | grep -q .; then
+        return 0
+    else
+        return 1
+    fi
+}
+function _action_fetch_font() {
+    line="$1"
+    _reolve_font_url "$line" || _die 3 "Cannot resolve namespace '$line' !" 
+    remote_url="$(_reolve_font_url "$line")"
+    item_name_hash="$(sha256sum <<< "$line" | cut -c1-8)"
+    item_basename="$(basename "$remote_url")"
+    local_path="$config_fontsdir/${item_name_hash}-${item_basename}"
+    if _decider_skip_download_task "$local_path"; then
+        echo "[INFO] Skipping '$local_path' for it is still very new"
+        return 0
+    fi
+    echo "Fetching '$remote_url' ..."
+    echo "local_path=$local_path"
+    curl --location --retry 40 --retry-max-time 60 --retry-all-errors -f "$remote_url" > "$local_path" || _die 4 "Failed downloading '$remote_url' !"
+    # echo curl --location --retry 40 --retry-max-time 60 --retry-all-errors -f "$remote_url" '>' "$local_path"
+
+}
+while read -r line; do
+    mkdir -p "$config_fontsdir"
+    _action_fetch_font "$line"
+done <<< "$config_fontslist"
